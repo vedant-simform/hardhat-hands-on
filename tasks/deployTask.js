810632cddc4549b07d58f9baae05c5a6243f88dd
vedant-simform <vedant@simformsolutions.com>
2023-04-11 14:07:23 +0530

    Task Updation

--- tasks/deployTask.js
+++ tasks/deployTask.js
@@ -1,14 +1,12 @@
 const { hexStripZeros } = require("ethers/lib/utils");
 const { task } = require("hardhat/config");
 
-task("deploy", "Deploy your contract")
-  .addParam("token", "Token Address")
-  .setAction(async (taskArgs) => {
-    await hre.run("compile");
+task("deploy", "Deploy your contract").setAction(async (taskArgs) => {
+  await hre.run("compile");
 
-    const vest = await hre.ethers.getContractFactory("Vesting");
-    const afterDeploy = await vest.deploy(taskArgs.token);
+  const vest = await hre.ethers.getContractFactory("Vesting");
+  const afterDeploy = await vest.deploy();
 
-    await afterDeploy.deployed();
-    console.log("Contract Deplyed Successfully..  at ", afterDeploy.address);
-  });
+  await afterDeploy.deployed();
+  console.log("Contract Deplyed Successfully..  at ", afterDeploy.address);
+});
--- tasks/verifyTask.js
+++ tasks/verifyTask.js
@@ -1,20 +1,12 @@
 const { task } = require("hardhat/config");
 
-// task("verify-contract", "Verify smart contract")
-//   .addParam("address", "Enter deployed contract address")
-//   .setAction(async (taskArgs) => {
-
-//   });
-
-task("verify-etherscan", "Verify deployed contract on Etherscan")
+task("verify-task", "Verify deployed contract on Etherscan")
   .addParam("contractaddress", "Contract address deployed")
-  .addParam("tokenvalue", "Enter token address")
   .setAction(async (taskArgs) => {
     try {
       await hre.run("verify:verify", {
         address: taskArgs.contractaddress,
         contract: "contracts/vesting.sol:Vesting",
-        constructorArguments: [taskArgs.tokenvalue],
       });
     } catch ({ message }) {
       console.error(message);
